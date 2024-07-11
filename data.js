22f76b0dfe807f70f661419846bd66f2cda825ce
syedaju398@gmail.com <syedaju398@gmail.com>
2024-07-11 16:01:04 +0530

    task-9 pagination

---
+++ data.js
@@ -0,0 +1,504 @@
+const data = [
+    {
+      "id": "1",
+      "name": "Alex Johnson",
+      "email": "alex.johnson@example.com"
+    },
+    {
+      "id": "2",
+      "name": "Bella Smith",
+      "email": "bella.smith@example.com"
+    },
+    {
+      "id": "3",
+      "name": "Charlie Brown",
+      "email": "charlie.brown@example.com"
+    },
+    {
+      "id": "4",
+      "name": "Diana Prince",
+      "email": "diana.prince@example.com"
+    },
+    {
+      "id": "5",
+      "name": "Edward Cullen",
+      "email": "edward.cullen@example.com"
+    },
+    {
+      "id": "6",
+      "name": "Fiona Gallagher",
+      "email": "fiona.gallagher@example.com"
+    },
+    {
+      "id": "7",
+      "name": "George Weasley",
+      "email": "george.weasley@example.com"
+    },
+    {
+      "id": "8",
+      "name": "Hermione Granger",
+      "email": "hermione.granger@example.com"
+    },
+    {
+      "id": "9",
+      "name": "Isaac Newton",
+      "email": "isaac.newton@example.com"
+    },
+    {
+      "id": "10",
+      "name": "Jack Sparrow",
+      "email": "jack.sparrow@example.com"
+    },
+    {
+      "id": "11",
+      "name": "Kelly Kapoor",
+      "email": "kelly.kapoor@example.com"
+    },
+    {
+      "id": "12",
+      "name": "Leonardo da Vinci",
+      "email": "leonardo.davinci@example.com"
+    },
+    {
+      "id": "13",
+      "name": "Mona Lisa",
+      "email": "mona.lisa@example.com"
+    },
+    {
+      "id": "14",
+      "name": "Nathan Drake",
+      "email": "nathan.drake@example.com"
+    },
+    {
+      "id": "15",
+      "name": "Olivia Pope",
+      "email": "olivia.pope@example.com"
+    },
+    {
+      "id": "16",
+      "name": "Paul Atreides",
+      "email": "paul.atreides@example.com"
+    },
+    {
+      "id": "17",
+      "name": "Quincy Jones",
+      "email": "quincy.jones@example.com"
+    },
+    {
+      "id": "18",
+      "name": "Rachel Green",
+      "email": "rachel.green@example.com"
+    },
+    {
+      "id": "19",
+      "name": "Samuel L. Jackson",
+      "email": "samuel.jackson@example.com"
+    },
+    {
+      "id": "20",
+      "name": "Tina Fey",
+      "email": "tina.fey@example.com"
+    },
+    {
+      "id": "21",
+      "name": "Uma Thurman",
+      "email": "uma.thurman@example.com"
+    },
+    {
+      "id": "22",
+      "name": "Vin Diesel",
+      "email": "vin.diesel@example.com"
+    },
+    {
+      "id": "23",
+      "name": "Wade Wilson",
+      "email": "wade.wilson@example.com"
+    },
+    {
+      "id": "24",
+      "name": "Xander Harris",
+      "email": "xander.harris@example.com"
+    },
+    {
+      "id": "25",
+      "name": "Yvonne Strahovski",
+      "email": "yvonne.strahovski@example.com"
+    },
+    {
+      "id": "26",
+      "name": "Zara Larsson",
+      "email": "zara.larsson@example.com"
+    },
+    {
+      "id": "27",
+      "name": "Albert Einstein",
+      "email": "albert.einstein@example.com"
+    },
+    {
+      "id": "28",
+      "name": "Bruce Wayne",
+      "email": "bruce.wayne@example.com"
+    },
+    {
+      "id": "29",
+      "name": "Clara Oswald",
+      "email": "clara.oswald@example.com"
+    },
+    {
+      "id": "30",
+      "name": "David Tennant",
+      "email": "david.tennant@example.com"
+    },
+    {
+      "id": "31",
+      "name": "Emma Watson",
+      "email": "emma.watson@example.com"
+    },
+    {
+      "id": "32",
+      "name": "Frank Castle",
+      "email": "frank.castle@example.com"
+    },
+    {
+      "id": "33",
+      "name": "Gandalf Grey",
+      "email": "gandalf.grey@example.com"
+    },
+    {
+      "id": "34",
+      "name": "Harry Potter",
+      "email": "harry.potter@example.com"
+    },
+    {
+      "id": "35",
+      "name": "Irene Adler",
+      "email": "irene.adler@example.com"
+    },
+    {
+      "id": "36",
+      "name": "John Doe",
+      "email": "john.doe@example.com"
+    },
+    {
+      "id": "37",
+      "name": "Katniss Everdeen",
+      "email": "katniss.everdeen@example.com"
+    },
+    {
+      "id": "38",
+      "name": "Lara Croft",
+      "email": "lara.croft@example.com"
+    },
+    {
+      "id": "39",
+      "name": "Michael Scott",
+      "email": "michael.scott@example.com"
+    },
+    {
+      "id": "40",
+      "name": "Nancy Drew",
+      "email": "nancy.drew@example.com"
+    },
+    {
+      "id": "41",
+      "name": "Oscar Wilde",
+      "email": "oscar.wilde@example.com"
+    },
+    {
+      "id": "42",
+      "name": "Peter Parker",
+      "email": "peter.parker@example.com"
+    },
+    {
+      "id": "43",
+      "name": "Quentin Tarantino",
+      "email": "quentin.tarantino@example.com"
+    },
+    {
+      "id": "44",
+      "name": "Rosie Huntington",
+      "email": "rosie.huntington@example.com"
+    },
+    {
+      "id": "45",
+      "name": "Steve Rogers",
+      "email": "steve.rogers@example.com"
+    },
+    {
+      "id": "46",
+      "name": "Tony Stark",
+      "email": "tony.stark@example.com"
+    },
+    {
+      "id": "47",
+      "name": "Ursula K. Le Guin",
+      "email": "ursula.leguin@example.com"
+    },
+    {
+      "id": "48",
+      "name": "Victor Frankenstein",
+      "email": "victor.frankenstein@example.com"
+    },
+    {
+      "id": "49",
+      "name": "Willy Wonka",
+      "email": "willy.wonka@example.com"
+    },
+    {
+      "id": "50",
+      "name": "Xena Warrior",
+      "email": "xena.warrior@example.com"
+    },
+    {
+      "id": "51",
+      "name": "Yoda Master",
+      "email": "yoda.master@example.com"
+    },
+    {
+      "id": "52",
+      "name": "Zeus Olympian",
+      "email": "zeus.olympian@example.com"
+    },
+    {
+      "id": "53",
+      "name": "Arthur Dent",
+      "email": "arthur.dent@example.com"
+    },
+    {
+      "id": "54",
+      "name": "Bilbo Baggins",
+      "email": "bilbo.baggins@example.com"
+    },
+    {
+      "id": "55",
+      "name": "Cersei Lannister",
+      "email": "cersei.lannister@example.com"
+    },
+    {
+      "id": "56",
+      "name": "Draco Malfoy",
+      "email": "draco.malfoy@example.com"
+    },
+    {
+      "id": "57",
+      "name": "Eddard Stark",
+      "email": "eddard.stark@example.com"
+    },
+    {
+      "id": "58",
+      "name": "Frodo Baggins",
+      "email": "frodo.baggins@example.com"
+    },
+    {
+      "id": "59",
+      "name": "Gollum Sméagol",
+      "email": "gollum.smeagol@example.com"
+    },
+    {
+      "id": "60",
+      "name": "Hannibal Lecter",
+      "email": "hannibal.lecter@example.com"
+    },
+    {
+      "id": "61",
+      "name": "Indiana Jones",
+      "email": "indiana.jones@example.com"
+    },
+    {
+      "id": "62",
+      "name": "James Bond",
+      "email": "james.bond@example.com"
+    },
+    {
+      "id": "63",
+      "name": "Katara Waterbender",
+      "email": "katara.waterbender@example.com"
+    },
+    {
+      "id": "64",
+      "name": "Luke Skywalker",
+      "email": "luke.skywalker@example.com"
+    },
+    {
+      "id": "65",
+      "name": "Matilda Wormwood",
+      "email": "matilda.wormwood@example.com"
+    },
+    {
+      "id": "66",
+      "name": "Neo TheOne",
+      "email": "neo.theone@example.com"
+    },
+    {
+      "id": "67",
+      "name": "Oprah Winfrey",
+      "email": "oprah.winfrey@example.com"
+    },
+    {
+      "id": "68",
+      "name": "Picard Jean-Luc",
+      "email": "picard.jeanluc@example.com"
+    },
+    {
+      "id": "69",
+      "name": "Rorschach Watchmen",
+      "email": "rorschach.watchmen@example.com"
+    },
+    {
+      "id": "70",
+      "name": "Sherlock Holmes",
+      "email": "sherlock.holmes@example.com"
+    },
+    {
+      "id": "71",
+      "name": "Tyrion Lannister",
+      "email": "tyrion.lannister@example.com"
+    },
+    {
+      "id": "72",
+      "name": "Uhtred Ragnarson",
+      "email": "uhtred.ragnarson@example.com"
+    },
+    {
+      "id": "73",
+      "name": "Van Helsing",
+      "email": "van.helsing@example.com"
+    },
+    {
+      "id": "74",
+      "name": "Wolverine Logan",
+      "email": "wolverine.logan@example.com"
+    },
+    {
+      "id": "75",
+      "name": "Xavier Charles",
+      "email": "xavier.charles@example.com"
+    },
+    {
+      "id": "76",
+      "name": "Ygritte Wildling",
+      "email": "ygritte.wildling@example.com"
+    },
+    {
+      "id": "77",
+      "name": "Zorro Fox",
+      "email": "zorro.fox@example.com"
+    },
+    {
+      "id": "78",
+      "name": "Hailie Haag",
+      "email": "Edythe.Lind59@hotmail.com"
+    },
+    {
+      "id": "79",
+      "name": "Marcelina Schuppe",
+      "email": "Kip.Cummerata77@gmail.com"
+    },
+    {
+      "id": "80",
+      "name": "Ezra Lang",
+      "email": "Garret.Swaniawski@hotmail.com"
+    },
+    {
+      "id": "81",
+      "name": "Carey Kiehn",
+      "email": "Sabrina70@gmail.com"
+    },
+    {
+      "id": "82",
+      "name": "Carole Aufderhar",
+      "email": "Efrain.Schamberger77@hotmail.com"
+    },
+    {
+      "id": "83",
+      "name": "Fanny Beatty",
+      "email": "Ewald28@yahoo.com"
+    },
+    {
+      "id": "84",
+      "name": "Mina Lubowitz",
+      "email": "Vincenzo.Herman@gmail.com"
+    },
+    {
+      "id": "85",
+      "name": "Anabelle Thiel",
+      "email": "Amely.Renner@hotmail.com"
+    },
+    {
+      "id": "86",
+      "name": "Gerry Homenick",
+      "email": "Ada_Von54@hotmail.com"
+    },
+    {
+      "id": "87",
+      "name": "Domenico Maggio",
+      "email": "Freeman.Stamm@yahoo.com"
+    },
+    {
+      "id": "88",
+      "name": "Mauricio Bradtke III",
+      "email": "Alford83@yahoo.com"
+    },
+    {
+      "id": "89",
+      "name": "Bulah Yost III",
+      "email": "Nicolas_Schimmel6@yahoo.com"
+    },
+    {
+      "id": "90",
+      "name": "Orville Dibbert DDS",
+      "email": "Bryana_Herzog@yahoo.com"
+    },
+    {
+      "id": "91",
+      "name": "Autumn Osinski",
+      "email": "Davin8@hotmail.com"
+    },
+    {
+      "id": "92",
+      "name": "Levi Gibson",
+      "email": "Deangelo69@gmail.com"
+    },
+    {
+      "id": "93",
+      "name": "Reva McKenzie",
+      "email": "Bret40@hotmail.com"
+    },
+    {
+      "id": "94",
+      "name": "Jensen Pfannerstill",
+      "email": "Dax_Marquardt@hotmail.com"
+    },
+    {
+      "id": "95",
+      "name": "Tyrese Larson MD",
+      "email": "Cecelia85@yahoo.com"
+    },
+    {
+      "id": "96",
+      "name": "Dayne Sanford",
+      "email": "Tyrique_Waters16@hotmail.com"
+    },
+    {
+      "id": "97",
+      "name": "Jeramy Steuber",
+      "email": "Catalina.Stehr24@yahoo.com"
+    },
+    {
+      "id": "98",
+      "name": "Madge Bogisich",
+      "email": "Joey.Marquardt50@gmail.com"
+    },
+    {
+      "id": "99",
+      "name": "Casimer MacGyver",
+      "email": "Julius.Bednar96@yahoo.com"
+    },
+    {
+      "id": "100",
+      "name": "Arlene Kassulke",
+      "email": "Josiane_Smith@yahoo.com"
+    }
+  ]
+  
+  export {data}
